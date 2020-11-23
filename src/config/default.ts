112019a20f26764c5fe11eb509203186ad183820
Carlos Santino Zaracho Simonetto <santino.zaracho@flydevs.com>
2020-11-23 19:22:27 -0300

    newe

--- src/config/default.ts
+++ src/config/default.ts
@@ -1,11 +1,16 @@
 export default {
-  port: process.env.PORT ?? 8000,
+  port: process.env.PORT ?? 3000,
   env: process.env.NODE_ENV ?? 'development',
   serverAddress: process.env.SERVER_ADDRESS,
   database: {
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-    host: process.env.DB_HOST,
+    user: 'root',
+    password: '1234',
+    database: 'devops',
+    host: '35.223.194.230',
   },
 }
+
+// user: process.env.DB_USER,
+// password: process.env.DB_PASSWORD,
+// database: process.env.DB_NAME,
+// host: process.env.DB_HOST,
