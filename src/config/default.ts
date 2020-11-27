065fa85b97baef00c116b4a12e6f24574c30ee52
Carlos Santino Zaracho Simonetto <40505201+santinozaracho@users.noreply.github.com>
2020-11-26 21:38:28 -0300

    Update default.ts

--- src/config/default.ts
+++ src/config/default.ts
@@ -3,14 +3,11 @@ export default {
   env: process.env.NODE_ENV ?? 'development',
   serverAddress: process.env.SERVER_ADDRESS,
   database: {
-    user: 'root',
-    password: '1234',
-    database: 'devops',
-    host: '127.0.0.1',
+        user: process.env.DB_USER,
+        password: process.env.DB_PASSWORD,
+        database: process.env.DB_NAME,
+        host: process.env.DB_HOST,
   },
 }
 
-// user: process.env.DB_USER,
-// password: process.env.DB_PASSWORD,
-// database: process.env.DB_NAME,
-// host: process.env.DB_HOST,
+
