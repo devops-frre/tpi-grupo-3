8482ebbfb26031d426a258c38d4d4f2d14487ee5
Gerardo Enrique <geraenrique97@hotmail.com>
2020-11-29 21:53:12 -0300

    Added test and pipeline step for testing

---
+++ src/features/pet/repositories/PetRepository.spec.js
@@ -0,0 +1,10 @@
+import { PetRepository } from "./";
+
+describe('PetRepository', () => {
+  it('should get all pet instances with findAll', () => {});
+  it('should return a pet given an userId with findById', () => {});
+  it('should create a pet given petObject with create function', () => {});
+  it('should update a pet given an id with updateById', () => {});
+  it('should delete a pet given an id with deleteById', () => {});
+
+});
---
+++ src/routes/pets/pets.spec.js
@@ -0,0 +1,8 @@
+import { petRouter } from "./";
+
+describe('petRouter', () => {
+  it('should return all instances when post method on "/" is requested', () => {});
+  it('should return a pet when get method on "/:id" is requested', () => {});
+  it('should call petService.create when post method on "/" is requested', () => {});
+  it('should update pet given an id when put method on "/:id" is requested', () => {});
+})
